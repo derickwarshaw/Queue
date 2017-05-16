acacb8da3871d31b7f114b4daedebb2ab2706929
Josh Crowe <josh@crowes.co>
2017-05-16 14:54:58 +0100

    Working on sequence methods in seperate repository.

--- modules/Sequence.js
+++ modules/Sequence.js
@@ -2,72 +2,125 @@ module.exports = dependencyInjection => {
 
   const Utility = dependencyInjection[0];
 
-   function Sequence (sequenceQuery) {
-      this.sequenceStatement = sequenceQuery;
-   }
-   Sequence.prototype.getSequence = function () {
-     return this.sequenceStatement;
-   }
+  function Sequence (sequenceIdentifier, sequenceOperand, sequenceMarker) {
+    this.sequenceSettings = {
+      availableMarkers: ['?'],
+      availableIdentifiers: ['SELECT', 'UPDATE', 'DELETE'],
+      availableOperands: ['*', 'TOP']
+    }
 
-   Sequence.prototype.into = function (intoTable, intoColumns) {
-      this.sequenceStatement += ` INTO ${intoTable}(`;
+    // "?" - Used to Mark Input Placeholder
+    if (this.sequenceSettings.availableMarkers.includes(sequenceMarker)) {
+      this.sequenceMarker = sequenceMarker;
+    }
 
-      for (let i = 0; i < intoColumns.length; i++) {
-         this.sequenceStatement += `${intoColumns[i]}, `;
-      }
+    // SELECT, UPDATE, DELETE,
+    if (this.sequenceSettings.availableIdentifiers.includes(sequenceIdentifier)) {
+      this.sequenceIdentifier = sequenceIdentifier;
+    }
+  }
+  Sequence.prototype.getSequence = function () {
+    let sequenceParticles = [];
 
-    this.sequenceStatement = `${this.sequenceStatement.substring(0, this.sequenceStatement.lastIndexOf(", "))})`;
+    for (sequenceParticle in this) {
+      sequenceParticles.push(this);
+    }
 
+    return sequenceParticles.join(' ');
+  }
+
+
+
+  /* ------------- Operans --------------------------- */
+
+  Sequence.prototpe.all = function () {
+    this.sequenceOperand = "*";
     return this;
-   }
-   Sequence.prototype.set = function (setColumn) {
-      if (this.seuqenceStatement.includes("SET")) {
-         this.sequenceStatement += ` ${setColumn}`;
-      } else {
-         this.sequenceStatement += ` SET ${setColumn}`;
-      }
+  }
+  Sequence.prototype.top = function (topNumber) {
+    this.sequenceOperand = `TOP (${topNumber})`;
+    return this;
+  }
+  Sequence.prototype.only = function (onlyColumns) {
+    this.sequenceOperand = onlyColumns.join(', ');
+    return this;
+  }
+  Sequence.prototype.into = function (intoTable, intoColumns) {
+    this.sequenceTable = `INTO ${intoTable}`;
+    this.sequenceColumns = `(${intoColumns.join(', ')})`;
+    return this;
+  }
 
-      return this;
-   }
 
 
-   Sequence.prototype.from = function (fromTable) {
-      this.sequenceStatement += ` FROM ${fromTable}`;
 
-      return this;
-   }
-   Sequence.prototype.values = function (valuesArrayLength) {
-      this.sequenceStatement += ` VALUES(`;
+  /* -------------- Operation -------------- */
 
-      for (let i = 0; i < valuesArrayLength; i++) {
-          this.sequenceStatement += `?, `;
-      }
+  Sequenece.prototype.from = function (fromTable) {
+    this.sequenceOperation = "FROM";
+    this.sequenceTable = fromTable;
+  }
 
-      this.sequenceStatement += `)`;
-      this.sequenceStatement = this.sequenceStatement.replace(', )', ')');
+  // .set([[Column1, Value1], [Column2, Value2]])
+  Sequence.prototype.set = function (setColumns) {
+    this.sequenceOperation = "SET";
+    this.sequenceSets = new Map();
 
-      return this;
-   }
+    for (let i = 0; i < setColumns.length; i++) {
+      this.sequenceSets.set(setColumns[i][0], setColumns[i][1]);
+    }
+  }
 
-   Sequence.prototype.where = function (whereCondition) {
-      if (this.sequenceStatement.match(/WHERE/g || []) > 0) {
-         this.sequenceStatement += ` AND WHERE ${whereCondition};`
-      } else {
-         this.sequenceStatement += ` WHERE ${whereCondition}`;
-      }
 
-      return this;
-   }
-   Sequence.prototype.equals = function () {
-      if (this.sequenceStatement.includes("SET")) {
-        this.sequenceStatement += `= ?, `;
-        this.sequenceStatement = this.sequenceStatement.replace(', )', ')');
-      } else {
-        this.sequenceStatement += ` = ?`;
-      }
 
-      return this;
-   }
+  //  Sequence.prototype.set = function (setColumn) {
+  //     if (this.seuqenceStatement.includes("SET")) {
+  //        this.sequenceStatement += ` ${setColumn}`;
+  //     } else {
+  //        this.sequenceStatement += ` SET ${setColumn}`;
+  //     }
+   //
+  //     return this;
+  //  }
+   //
+   //
+  //  Sequence.prototype.from = function (fromTable) {
+  //     this.sequenceStatement += ` FROM ${fromTable}`;
+   //
+  //     return this;
+  //  }
+  //  Sequence.prototype.values = function (valuesArrayLength) {
+  //     this.sequenceStatement += ` VALUES(`;
+   //
+  //     for (let i = 0; i < valuesArrayLength; i++) {
+  //         this.sequenceStatement += `?, `;
+  //     }
+   //
+  //     this.sequenceStatement += `)`;
+  //     this.sequenceStatement = this.sequenceStatement.replace(', )', ')');
+   //
+  //     return this;
+  //  }
+   //
+  //  Sequence.prototype.where = function (whereCondition) {
+  //     if (this.sequenceStatement.match(/WHERE/g || []) > 0) {
+  //        this.sequenceStatement += ` AND WHERE ${whereCondition};`
+  //     } else {
+  //        this.sequenceStatement += ` WHERE ${whereCondition}`;
+  //     }
+   //
+  //     return this;
+  //  }
+  //  Sequence.prototype.equals = function () {
+  //     if (this.sequenceStatement.includes("SET")) {
+  //       this.sequenceStatement += `= ?, `;
+  //       this.sequenceStatement = this.sequenceStatement.replace(', )', ')');
+  //     } else {
+  //       this.sequenceStatement += ` = ?`;
+  //     }
+   //
+  //     return this;
+  //  }
 
 
    return Sequence;
