fb7afc88b6113dcc57a6d17853c50d3434c708c2
Joshua Crowe <thejoshuacrowe@gmail.com>
2017-07-05 21:38:57 +0100

    Cleaned up request handler.

--- events/Request.js
+++ events/Request.js
@@ -10,27 +10,18 @@ const Translation = currentApplication.component('Translation');
 async function Request (requestObject) {
   "use strict";
 
-  // Step 1: Check if room & user exists.
     const requestedRoom = await currentDatabase.readRoom(requestObject.userLocation);
     const readUser = await currentDatabase.readUser(requestObject, "Id");
 
     if (requestedRoom && readUser) {
-
-        // Step 2: Create a client.
         await currentDatabase.writeClient(requestObject.userName, requestedRoom.RoomId);
-
     } else {
         throw Error(`${requestObject.userLocation} is not a registered room.`);
     }
 
-    // Step 3: Verify the client can be found.
-    const readClient = await currentDatabase.readClient(requestObject.userName);
-
-    // Step 4: Translate the client & the user.
-    const translatedClient = await Translation.client(readClient);
+    const translatedClient = await Translation.client(await currentDatabase.readClient(requestObject.userName));
     const translatedUser = await Translation.user(readUser);
 
-    // Step 5: Alter the clientId of the user.
     translatedUser.clientId = translatedClient.clientId;
     await currentDatabase.alterUser(translatedUser);
 
