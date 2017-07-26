f9e01c21be76b3528e1c5418f20993303a79ebbb
Josh Crowe <thejoshuacrowe@gmail.com>
2017-07-26 08:44:08 +0100

    Finished rewriting Board.js

--- public/scripts/Board.js
+++ public/scripts/Board.js
@@ -12,7 +12,7 @@ class Board {
     const $bindTargets = attendedTarget.query('.point');
 
     for (let i = 0; i < $bindTargets.length; i++) {
-      const bindInstance = new Point($bindTargets[i]);
+      const bindInstance = new Point($bindTargets.index(i));
 
       await bindInstance.bind();
 
@@ -34,7 +34,7 @@ class Board {
     const changePath = `${this.boardAddress}/api/system/distinctor/`;
 
     this.boardSocket.on('notif:change', changeData => {
-      changeHandler(changeData, changePath);
+      changeHandler(changeData, changePath + changeData.clientSystemDistinctor);
     });
   }
 
@@ -47,47 +47,47 @@ class Board {
   }
 }
 
-
 class Point {
-    constructor (pointTarget) {
-        this.pointTarget = pointTarget;
-        this.pointElements = new Map();
-        this.pointData = new Map();
-    }
+  constructor (pointTarget) {
+    this.pointTarget = pointTarget;
     
-    get number () {
-        return parseInt(this.pointData.get("LeftHeader"), 10);
-    }
+    this.pointElements = new Map();
+    this.pointData = new Map();
+  }
+  
+  get number () {
+    return parseInt(this.pointData.get("LeftHeader"), 10);
+  }
+  
+  get status () {
+    return this.pointData.get("RightHeader");
+  }
+  
+  async bind () {
+    const $bindTarget = this.pointTarget;
     
-    get status () {
-        return this.pointData.get("RightHeader");
-    }
+    const $pointLeft = $bindTarget.find('.point-left');
+    const $pointLeftHeader = $pointLeft.find('p');
+    this.pointElements.set("Left", $pointLeft);
+    this.pointElements.set("LeftHeader", $pointLeftHeader);
+    this.pointData.set("LeftHeader", $pointLeftHeader.text);
     
-    async bind () {
-        const $bindTarget = this.pointTarget;
-        
-        const $pointLeft = $bindTarget.children('.point-left');
-        const $pointLeftHeader = $pointLeft.children('p');
-        this.pointElements.set("Left", $pointLeft);
-        this.pointElements.set("LeftHeader", $pointLeftHeader);
-        this.pointData.set("LeftHeader", $pointLeftHeader.text());
-        
-        const $pointRight = $bindTarget.children('.point-right');
-        const $pointRightHeader = $pointRight.children('p');
-        this.pointElements.set("Right", $pointRight);
-        this.pointElements.set("RightHeader", $pointRightHeader);
-        this.pointData.set("RightHeader", $pointRightHeader.text());
-    }
-
-    update (updateStatus) {
-      const $updateTarget = this.pointTarget;
-
-      $updateTarget.removeClass('available away busy');
-      $updateTarget.addClass(updateStatus);
-
-      const $rightHeader = this.pointElements.get("RightHeader");
-
-      $rightHeader.text(updateStatus);
-      this.pointData.set("RightHeader", updateStatus);
-    }
+    const $pointRight = $bindTarget.find('.point-right');
+    const $pointRightHeader = $pointRight.find('p');
+    this.pointElements.set("Right", $pointRight);
+    this.pointElements.set("RightHeader", $pointRightHeader);
+    this.pointData.set("RightHeader", $pointRightHeader.text);
+  }
+  
+  update (updateStatus) {
+    const $updateTarget = this.pointTarget;
+    
+    $updateTarget.removeClass(['available', 'busy', 'away']);
+    $updateTarget.addClass(updateStatus);
+    
+    const $pointRightHeader = this.pointElements.get("RightHeader");
+    
+    $pointRightHeader.text = updateStatus;
+    this.pointData.set("RightHeader", updateStatus);
+  }
 }
